c54c030ac288c1623706539586c31dde5807c059
meritxellavila <meriavila66@gmail.com>
2024-03-14 23:13:37 +0100

    rutas receta terminadas

--- routes/recetas.routes.js
+++ routes/recetas.routes.js
@@ -62,51 +62,48 @@ router.get("/:recetasId", async (req, res, next) => {
   }
 });
 
-//   //PUT "api/recetas/recetaId => actualiza/ modificar  receta por id
-//   router.put("/:recetaId", async (req, res, next) => {
-//     const { nombre,
-//       imagen,
-//       pasos,
-//       ingredientes,
-//       creadoPor,
-//       Opiniones,
-//       FavoritosBy,
-//     } = req.body;
+  //PUT "api/recetas/recetaId => actualiza/ modificar  receta por id
+  router.put("/:recetasId", async (req, res, next) => {
+    const { nombre,
+      imagen,
+      pasos,
+      ingredientes,
+      creadoPor,
+      Opiniones,
+      FavoritosBy,
+    } = req.body;
   
   
-//     try {
-//       const response = await Receta.findByIdAndUpdate(
-//         req.params.recetaId,
-//         {
-//           nombre,
-//           imagen,
-//           pasos,
-//           ingredientes,
-//           creadoPor,
-//           Opiniones,
-//           FavoritosBy,
-//         },
-//         { new: true }
-//       );
-//       res.status(202).json(response);
-//     } catch (error) {
-//       next(error);
-//     }
-//   });
+    try {
+      const response = await Recetas.findByIdAndUpdate(
+        req.params.recetasId,
+        {
+          nombre,
+          imagen,
+          pasos,
+          ingredientes,
+          creadoPor,
+          Opiniones,
+          FavoritosBy,
+        },
+        { new: true }
+      );
+      res.status(202).json(response);
+    } catch (error) {
+      next(error);
+    }
+  });
 
-//   // DELETE "/api/recetas/recetasId =>elimina una receta por id
-//   // router.delete("/:recetaId", async (req, res, next) => {
-//   //   try {
-//   //     await Recetas.findByIdAndDelete(req.params.revetaId);
-//   //     res.status(202).json({ message: "receta borrada" });
-//   //   } catch (error) {
-//   //     next(error);
-//   //   }
-//   // });
+  // DELETE "/api/recetas/recetasId =>elimina una receta por id
+  router.delete("/:recetasId", async (req, res, next) => {
+    try {
+      await Recetas.findByIdAndDelete(req.params.recetasId);
+      res.status(202).json({ message: "receta borrada" });
+    } catch (error) {
+      next(error);
+    }
+  });
 
 
 module.exports = router;
 
-// let variable = "patata"
-
-// let enMayuscula = variable.toUpperCase ()
